ab5f8b0e60f945d7fefe93841c68958087e79bce
GabiRmz2025 <gaby.rmzcas@gmail.com>
2025-06-06 05:42:46 -0600

    Se agregan Operadores y Tablas de Verdad
    En el ejercicio se agregan operadores aritméticos, lógicos y de comparación, y se crean tablas de verdad.

---
+++ Fundamentos.py
@@ -0,0 +1,52 @@
+if __name__ == "__main__":
+
+    #Este es un comentario de una sola línea
+    '''Es un comentario de varias líneas
+    son tres comillas al inicio y final de parrafo'''
+    
+    print("----- Operadores aritméticos -----")
+    # Ejercicios usando operadores aritméticos
+    print("operador suma, 5 + 6 =", 5 + 6)
+    print("operador resta, 9 - 8 =", 9 - 8)
+    print("operador multiplicación, 9 * 8 =", 9 * 8)
+    print("operador división, 440 / 4 =", 440 / 4)
+    print("operador división entero, 85 // 3 =", 85 // 3)
+    print("operador exponente, 6 ** 3=", 6 ** 3)
+    print("operador módulo, 27 % 5 =", 27 % 5)
+    print("operador cambio de signo, -( 4 - 6 ) =", -( 4 - 6 ))
+    print("varios operadores, ( (4 + 5) * 2 / 3) =", (4 + 5) * 2 / 3)
+    print("")
+    # Ejercicios usando operadores lógicos
+    # not=negación, or=disyunción, and=conjunción
+    print("")
+    print("----- Tablas de Verdad ----")    
+    print("Haciendo uso de los Operadores Lógicos (and, or, not)")
+    print("")
+    print("--- Operador And ---")
+    print("True and True, resulta =", True and True)
+    print("True and False, resulta =", True and False)
+    print("False and True, resulta =", False and True)
+    print("False and False, resulta =", False and False)
+    print("")
+    print("--- Operador Or ---")
+    print("True or True, resulta =", True or True)
+    print("True or False, resulta =", True or False)
+    print("False or True, resulta =", False or True)
+    print("False or False, resulta =", False or False)
+    print("Not de True and False, resulta =", True and False)
+    print("")
+    print("--- Operador Not ---")
+    print("Negación de True, resulta =", not True )
+    print("Negación de True or False, resulta =", not (True or False))
+    print("")
+    print("----- Operadores de Comparación -----")
+    print("")
+    #ejercicios usando operadores de comparación
+    print("Respuesta de 37.4 == 37.4: ", 37.4 == 37.4)
+    print("48 es != de 48.7, respuesta:", 48 != 48.7)
+    print("48.1 es menor que (<) 48, respuesta =", 48.1 < 48)
+    print("48.7 mas 0.5 es mayor que 48, respuesta: ", 48.7 > 48)
+    print("El resultado de ( 5 + 4 ) es mayor o igual que ( 3 * 3 ):", ( 5 + 4 ) >= ( 3 * 3 ))
+    print("Ejemplo completo: (True or (2 == 1 + 2)) == True: ", (True or (2 == 1 + 2)) == True)
+
+print
