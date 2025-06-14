0763c01b6fe0d81d27ce4eadb27f43e2692b6487
GabiRmz2025 <gaby.rmzcas@gmail.com>
2025-06-14 13:29:28 -0600

    Calculadora Python
    Archivo con el código que contiene el código de las operaciones de la calculadora

---
+++ Calc/operaciones.py
@@ -0,0 +1,26 @@
+#Ejemplo para crear paquetes con operaciones aritméticas
+
+def resta (*nums:float)-> float:
+    res = nums[0]
+    for valor in nums:
+        res -= valor
+    return res
+
+
+def suma (*nums:float)-> float:
+    res = nums[0]
+    for valor in nums:
+        res += valor
+    return res
+
+def mult (*nums:float)-> float:
+    res = 1
+    for valor in nums:
+        res *= valor
+    return res
+
+def div(n1:float, n2:float)-> float:
+    res = n1/n2
+    sobra = n1 % n2
+
+    return res, sobra
