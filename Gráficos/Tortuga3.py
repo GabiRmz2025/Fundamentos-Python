281e59dc5ddc24a04acb286409ae4f716f334982
GabiRmz2025 <gaby.rmzcas@gmail.com>
2025-06-28 15:10:33 -0600

    Actividad Turtle 3
    Figura Triangulo y Cuadro Cruzado

---
+++ Gráficos/Tortuga3.py
@@ -0,0 +1,47 @@
+#Actividad Turtle 3.Triangulo y Cuadro Cruzado
+
+from turtle import *
+import time
+
+left(90)
+
+forward(120)
+time.sleep(1)
+
+right(45)
+
+forward(120)
+time.sleep(1)
+
+right(90)
+
+forward(120)
+time.sleep(1)
+
+right(45)
+time.sleep(1)
+
+forward(120)
+time.sleep(1)
+
+right(125)
+
+forward(210)
+time.sleep(1)
+
+right(145)
+
+forward(170)
+time.sleep(1)
+
+right(145)
+
+forward(210)
+time.sleep(1)
+
+left(145)
+
+forward(175)
+time.sleep(1)
+
+time.sleep(3)
