511f91f00dba48a1bccded253ea2bcb44f1679fc
GabiRmz2025 <gaby.rmzcas@gmail.com>
2025-06-28 14:55:53 -0600

    1er. Ejercicio de Turtle
    Figuras Varias: Uso de métodos de movimiento, dibujo, tiempos, posiciones

---
+++ Gráficos/Complejo1.py
@@ -0,0 +1,29 @@
+#1er. Ejercicio de Turtle, Figuras Varias
+#Uso de métodos de movimiento, dibujo, tiempos, posiciones
+
+import turtle
+import time
+
+turtle.forward(100)
+time.sleep(1)
+
+turtle.left(90)
+time.sleep(1)
+
+turtle.backward(50)
+time.sleep(1)
+
+turtle.left(90)
+time.sleep(1)
+
+turtle.circle(50)
+time.sleep(1)
+
+turtle.penup()
+turtle.setx(-100)
+turtle.sety(-100)
+turtle.pendown()
+
+turtle.circle(40)
+
+time.sleep(10)
