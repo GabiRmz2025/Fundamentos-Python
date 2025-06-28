92b8762247a3f21284dfbfc2cbaa4db14c20d91d
GabiRmz2025 <gaby.rmzcas@gmail.com>
2025-06-28 15:12:12 -0600

    Actividad Turtle 6
    Figura de Hexágono

---
+++ Gráficos/Tortuga6.py
@@ -0,0 +1,39 @@
+#Actividad Turtle 6. Hexágono
+
+from turtle import *
+import time
+
+forward(100)
+time.sleep(1)
+
+right(120)
+time.sleep(1)
+
+backward(100)
+time.sleep(1)
+
+left(60)
+time.sleep(1)
+
+backward(100)
+time.sleep(1)
+
+left(60)
+time.sleep(1)
+
+backward(100)
+time.sleep(1)
+
+left(60)
+time.sleep(1)
+
+backward(100)
+time.sleep(1)
+
+left(60)
+time.sleep(1)
+
+backward(100)
+time.sleep(1)
+
+right(90)
