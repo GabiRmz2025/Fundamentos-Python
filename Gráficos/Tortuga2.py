231ac1c37d6e2b60a8fbf14ab083b0b52000e1da
GabiRmz2025 <gaby.rmzcas@gmail.com>
2025-06-28 15:10:00 -0600

    Actividad Turtle 2
    Figura: Triple Cuadrado cruzado

---
+++ Gráficos/Tortuga2.py
@@ -0,0 +1,40 @@
+#Actividad Turtle 2. Triple Cuadrado cruzado
+
+from turtle import *
+import time
+
+left(45)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+
+
+left(45)
+time.sleep(1)
+
+left(30)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+
+
+left(45)
+time.sleep(1)
+
+left(30)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+time.sleep(1)
